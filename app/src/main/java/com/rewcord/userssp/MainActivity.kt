de3ef88c62ca07286526f20672896fbd9763524a
Jorge Martínez Bernat <jorma29j@epsa.upv.es>
2022-08-27 15:39:10 +0200

    Test

---
+++ app/src/main/java/com/rewcord/userssp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rewcord.userssp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
